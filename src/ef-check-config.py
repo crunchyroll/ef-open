a3051c52d263db3b71b2d3dc8902470460ee484e
jyoullellation <jyoull@ellation.com>
2017-04-09 12:18:20 -0700

    Update copyrights to 2016-2017

--- src/ef-check-config.py
+++ src/ef-check-config.py
@@ -9,7 +9,7 @@ Exit codes:
   3 if some other error occurred
 
 
-Copyright 2016 Ellation, Inc.
+Copyright 2016-2017 Ellation, Inc.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
--- src/ef-generate.py
+++ src/ef-generate.py
@@ -12,7 +12,7 @@ environment, make it.
 If --commit flag is not set, only a dry run occurs; no changes are made.
 
 
-Copyright 2016 Ellation, Inc.
+Copyright 2016-2017 Ellation, Inc.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
