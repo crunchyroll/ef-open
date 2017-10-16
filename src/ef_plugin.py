2d7a4a186543df71b0dac8ba47d0a851406f3107
Dan Lutsch <dlutsch@ellation.com>
2017-10-16 16:53:45 -0700

    change logging statements to print to match existing pattern

--- src/ef_plugin.py
+++ src/ef_plugin.py
@@ -7,7 +7,6 @@ import sys
 
 logger = logging.getLogger(__name__)
 
-
 def ef_plugin(service_name):
   """
   Decorator for ef plugin classes. Any wrapped classes should contain a run() method which executes the plugin code.
@@ -65,16 +64,18 @@ def run_plugins(context_obj, boto3_clients):
 
   Args:
     context_obj (obj:EFContext): The EFContext object created by the service.
+    boto3_clients (dict): Dictionary of boto3 clients created by ef_utils.create_aws_clients()
   """
-  if not context_obj.commit:
-    logger.debug("Dry Run: Skipping plugin execution.")
-    return
+
+  def print_if_verbose(message):
+    if context_obj.verbose:
+      print(message)
 
   service_name = os.path.basename(sys.argv[0]).replace(".py", "")
   try:
     import plugins
   except ImportError:
-    logger.debug("No plugins detected. Skipping")
+    print_if_verbose("no plugins detected.")
     return
   else:
     for plugin_importer, plugin_name, plugin_ispkg in pkgutil.iter_modules(plugins.__path__):
@@ -87,7 +88,11 @@ def run_plugins(context_obj, boto3_clients):
               plugin_class = getattr(plugin_module, name)
               plugin_instance = plugin_class(context=context_obj, clients=boto3_clients)
               if plugin_instance.service == service_name:
-                try:
-                  plugin_instance.run()
-                except AttributeError:
-                  logger.error("Error executing plugin '{}'".format(modname))
+                print_if_verbose("plugin '{}' loaded".format(plugin_name))
+                if not context_obj.commit:
+                  print_if_verbose("dryrun: skipping plugin execution.")
+                else:
+                  try:
+                    plugin_instance.run()
+                  except AttributeError:
+                    print("error executing plugin '{}'".format(modname))
