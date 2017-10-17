b9aa9094c62c1d032c403b457f810117690effc2
Dan Lutsch <dlutsch@ellation.com>
2017-10-17 14:50:07 -0700

    add critical_alert_environments

--- plugins_available/newrelic/config.py
+++ plugins_available/newrelic/config.py
@@ -1,7 +1,8 @@
-# NewRelic admin api token, encrypted by KMS
+# NewRelic admin api token, encrypted by KMS. Any accounts tied to environments in alert_environments will need decrypt
+#  privileges to this key.
 encrypted_token = ""
 
-# Notification channels to be used for warning and critical alert levels
+# Notification channels in NewRelic to be used for warning and critical alert levels
 # ex. warning_channels = ["slack-warn"]
 warning_channels = []
 critical_channels = []
@@ -10,7 +11,15 @@ critical_channels = []
 # ex. alert_environments = ["staging", "prod"]
 alert_environments = []
 
-# Alert conditions to be used for each service along with default thresholds
+# Environments which will trigger critical alert channels when a critical condition threshold is passed. Otherwise,
+# warning channels will be used.
+# ex. critical_alert_environments = ["prod"]
+critical_alert_environments = []
+
+# Alert conditions to be used for each service along with default thresholds. This currently supports infrastructure
+# alerts filtering based on the ec2 name tags created by ef-cf. The key in the conditions dict is the condition name
+# that will appear in NewRelic. The sr_name value is what is used inside the service registry to set service-specific
+# condition thresholds that differ from the defaults. See the readme for examples.
 conditions = {
   'memoryUsed': {
     'event_type': 'SystemSample',
