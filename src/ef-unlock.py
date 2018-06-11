5cefc941f3b01c97a599dabb915b809990dbacc0
Dan Lutsch <dlutsch@ellation.com>
2018-06-10 23:39:19 -0700

    v1 almost complete..

--- src/ef-unlock.py
+++ src/ef-unlock.py
@@ -1,46 +1,17 @@
 import os.path
 import re
-import sqlite3
 import hashlib
 import shutil
 import json
 from collections import OrderedDict
+import sqlite3
+import sys
+
 import boto3
 import botocore.exceptions
 
 from ef_site_config import EFSiteConfig
 from ef_utils import kms_decrypt
-
-topdir = './configs'
-
-# The arg argument for walk, and subsequently ext for step
-exten = '.parameters.json'
-
-service = re.compile('\./configs/(.+)/parameters')
-encrypted_start_char = '_'
-
-files = []
-
-def step(ext, dirname, names):
-    ext = ext.lower()
-
-    for name in names:
-        if name.lower().endswith(ext):
-            files.append({
-                "service": service.match(dirname).group(1),
-                "params_dir": dirname,
-                "filepath": os.path.join(dirname, name),
-                "filename": name
-            })
-            filepath = os.path.join(dirname, name)
-
-            # Make copy named .filename.locked
-            # This will be restored when ef-lock is called as long as no changes have taken place on this file
-            locked_copy = ".{}.locked".format(name)
-            locked_copy_filepath = os.path.join(dirname, locked_copy)
-            shutil.copy2(filepath, locked_copy_filepath)
-
-            # Unencrypt file
 
 
 def create_kms_clients():
@@ -70,61 +41,131 @@ def create_kms_clients():
     return kms
 
 
-def is_encrypted(param_value):
-    if param_value.startswith("{{aws:kms:decrypt"):
-        return True
-    else:
-        return False
-
-def decrypt_secret_file(file_path, clients):
-  """
-  Generate a parameter files with it's secrets encrypted in KMS
-  Args:
-      file_path (string): Path to the parameter file to be encrypted
-      pattern (string): Pattern to do fuzzy string matching
-      service (string): Service to use KMS key to encrypt file
-      clients (dict): KMS AWS client that has been instantiated
-  Returns:
-      None
-  Raises:
-    IOError: If the file does not exist
-  """
-  with open(file_path) as json_file:
-    data = json.load(json_file, object_pairs_hook=OrderedDict)
-  for env, params in data["params"].items():
-      if env in clients.keys():
-          for key, value in params.items():
-              if key.startswith('_') and is_encrypted(value):
-                  print()
-
-    # if pattern in key:
-    #   if "aws:kms:decrypt" in value:
-    #     print("Found match, key {} but value is encrypted already; skipping...".format(key))
-    #   else:
-    #     print("Found match, encrypting key {}".format(key))
-    #     encrypted_password = ef_utils.kms_encrypt(clients['kms'], service, environment, value)
-    #     data["params"][environment][key] = format_secret(encrypted_password)
-    #     changed = True
+def decrypt_file(file_path, clients, encryption_char='_'):
+    """
+    Generate a parameter files with it's secrets encrypted in KMS
+    Args:
+        file_path (string): Path to the parameter file to be encrypted
+        clients (dict): KMS AWS client that has been instantiated
+        encryption_char (string): The symbol/text preceding encypted values
+    Returns:
+        None
+    Raises:
+      IOError: If the file does not exist
+    """
+    changed = False
+    with open(file_path) as json_file:
+        data = json.load(json_file, object_pairs_hook=OrderedDict)
+    for env, params in data["params"].items():
+        if env in clients.keys():
+            for key, value in params.items():
+                if key.startswith(encryption_char) and value.startswith("{{aws:kms:decrypt"):
+                    encrypted_value = "".join(value.strip('{}').split(',')[1:])  # strip away ef-open lookup symbols
+                    try:
+                        decrypted_value = kms_decrypt(clients[env], encrypted_value)
+                        data['params'][env][key] = decrypted_value
+                        changed = True
+                    except: # TODO: Find botocore exception for missing permissions
+                        pass
+    if changed:
+        with open(file_path, "w") as encrypted_file:
+            json.dump(data, encrypted_file, indent=2, separators=(',', ': '))
+            # Writing new line here so it conforms to WG14 N1256 5.1.1.1 (so github doesn't complain)
+            encrypted_file.write("\n")
 
 
-test_file = 'configs/cr-web/parameters/config_prod.yml.parameters.json'
-kms = create_kms_clients()
-decrypt_secret_file(test_file, kms)
+def file_as_bytes(file):
+    with file:
+        return file.read()
 
 
-# Start the walk
-# os.path.walk(topdir, step, exten)
+def find_param_files(configdir, parameter_exten):
+    service = re.compile('\./configs/(.+)/parameters')
+    files = []
 
-# Store MD5Sum of all unencrypted files in sqlite
+    def step(ext, dirname, names):
+        ext = ext.lower()
 
-# Add configs/ to .gitignore
+        for name in names:
+            if name.lower().endswith(ext):
+                files.append({
+                    "service": service.match(dirname).group(1),
+                    "params_dir": dirname,
+                    "filepath": os.path.join(dirname, name),
+                    "filename": name
+                })
 
-# TODO: Validation to make sure ef-unlock isn't run twice in a row
+    os.path.walk(configdir, step, parameter_exten)
+    return files
 
-# TODO: Handling for if a user has a profile matching the account, but is missing decrypt permissions
 
-# with open(os.path.join(dirname, name)) as f:
-#     data = f.read()
-#     print(hashlib.md5(data).hexdigest())
+def create_locked_copy(filename, parent_dir):
+    source_filepath = os.path.join(parent_dir, filename)
+    locked_copy = ".{}.locked".format(filename)
+    locked_copy_filepath = os.path.join(parent_dir, locked_copy)
+    shutil.copy2(source_filepath, locked_copy_filepath)
 
-# Ef-lock should be able to to run either after ef-unlock (where it cleans everything up) or on its own
+
+def get_md5sum(filepath):
+    with open(filepath) as f:
+        data = f.read()
+    return hashlib.md5(data).hexdigest()
+
+
+def print_if_verbose(message):
+    if verbose:
+        print(message)
+
+
+def main():
+
+    configdir = './configs/cr-web'
+    parameter_exten = '.parameters.json'
+    encrypted_start_char = '_'
+    verbose = True
+
+    # TODO: Colorize these
+    if not os.path.isfile('ef_site_config.yml'):
+        print("ef-unlock must be run at the root of the repo. exiting.")
+        sys.exit(1)
+
+    if os.path.isfile('.ef-lock'):
+        print("ef-unlock has already been executed on this repo. lock with ef-lock before trying again.")
+        sys.exit(1)
+
+    # Create checksums db
+    conn = sqlite3.connect('.ef-lock')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE checksums
+                 (FilePath text NOT NULL, MD5 text NOT NULL, PRIMARY KEY (FilePath))''')
+
+
+    # Create kms clients for each account
+    kms_clients = create_kms_clients()
+
+    # Create collection of all param files
+    files = find_param_files(configdir, parameter_exten)
+
+    for file in files:
+
+        # Create locked copy, ef-lock will restore this file later if the content of the params file is unchanged
+        create_locked_copy(file['filename'], file['params_dir'])
+
+        # Decrypt
+        decrypt_file(file['filepath'], kms_clients, encrypted_start_char)
+
+        # Get decrypted copy checksum. This will be used by ef-lock
+        checksum = get_md5sum(file['filepath'])
+
+        # Add configs/ to .gitignore
+
+
+        # Save checksums to .ef-lock
+        c.execute("INSERT INTO checksums VALUES (?, ?)", (file['filepath'], checksum))
+        conn.commit()
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
