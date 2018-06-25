2eee7c18201ca5b8d1a37f759444e48156403274
Thomas Overton <toverton@ellation.com>
2018-06-25 10:44:01 -0700

    support yaml and json; resolve templates with no param files

--- src/ef-resolve-config.py
+++ src/ef-resolve-config.py
@@ -26,26 +26,29 @@ from __future__ import print_function
 
 import argparse
 from ef_config import EFConfig
-import json
+import yaml
 from os.path import abspath, dirname, normpath
 import sys
 
 from ef_template_resolver import EFTemplateResolver
 from ef_utils import get_account_alias
 
+
 class Context:
-  def __init__(self, profile, region, env, service, template_path, verbose):
+  def __init__(self, profile, region, env, service, template_path, no_params, verbose):
     self.profile = profile
     self.region = region
     self.env = env
     self.service = service
     self.template_path = template_path
+    self.no_params = no_params
     self.param_path = self.template_path.replace("/templates", "/parameters") + EFConfig.PARAMETER_FILE_SUFFIX
     self.verbose = verbose
 
   def __str__(self):
     return("profile: {}\nregion: {}\nenv: {}\nservice: {}\ntemplate_path: {}\nparam_path: {}\n".format(
-      self.profile, self.region, self.env, self.service, self.template_path, self.param_path, self.verbose))
+           self.profile, self.region, self.env, self.service, self.template_path, self.param_path, self.verbose))
+
 
 def handle_args_and_set_context(args):
   """
@@ -57,17 +60,22 @@ def handle_args_and_set_context(args):
   parser = argparse.ArgumentParser()
   parser.add_argument("env", help="environment")
   parser.add_argument("path_to_template", help="path to the config template to process")
+  parser.add_argument("--no_params", help="disable loading values from params file", action="store_true", default=False)
   parser.add_argument("--verbose", help="Output extra info", action="store_true", default=False)
   parsed = vars(parser.parse_args(args))
   path_to_template = abspath(parsed["path_to_template"])
   service = path_to_template.split('/')[-3]
 
   return Context(
-    get_account_alias(parsed["env"]),
-    EFConfig.DEFAULT_REGION, parsed["env"],
-    service, path_to_template,
-    parsed["verbose"]
+      get_account_alias(parsed["env"]),
+      EFConfig.DEFAULT_REGION,
+      parsed["env"],
+      service,
+      path_to_template,
+      parsed["no_params"],
+      parsed["verbose"]
   )
+
 
 def merge_files(context):
   """
@@ -77,10 +85,10 @@ def merge_files(context):
     context: a populated context object
   """
   resolver = EFTemplateResolver(
-    profile = context.profile,
-    region = context.region,
-    env = context.env,
-    service = context.service
+      profile=context.profile,
+      region=context.region,
+      env=context.env,
+      service=context.service
   )
 
   try:
@@ -90,25 +98,26 @@ def merge_files(context):
   except IOError as error:
     raise IOError("Error loading template file: {} {}".format(context.template_path, repr(error)))
 
-  try:
-    with open(context.param_path, 'r') as f:
-      param_body = f.read()
-      f.close()
-  except IOError as error:
-    raise IOError("Error loading param file: {} {}".format(context.param_path, repr(error)))
+  if context.no_params is False:
+    try:
+      with open(context.param_path, 'r') as f:
+        param_body = f.read()
+        f.close()
+    except IOError as error:
+      raise IOError("Error loading param file: {} {}".format(context.param_path, repr(error)))
 
-  dest = json.loads(param_body)["dest"]
+    dest = yaml.safe_load(param_body)["dest"]
 
-  # if 'dest' for the current object contains an 'environments' list, check it
-  if dest.has_key("environments"):
-    if not resolver.resolved["ENV_SHORT"] in dest["environments"]:
-      print("Environment: {} not enabled for {}".format(
-        resolver.resolved["ENV_SHORT"], context.template_path)
-      )
-      return
+    # if 'dest' for the current object contains an 'environments' list, check it
+    if "environments" in dest:
+      if not resolver.resolved["ENV_SHORT"] in dest["environments"]:
+        print("Environment: {} not enabled for {}".format(resolver.resolved["ENV_SHORT"], context.template_path))
+        return
 
-  # Process the template_body - apply context + parameters
-  resolver.load(template_body, param_body)
+    # Process the template_body - apply context + parameters
+    resolver.load(template_body, param_body)
+  else:
+    resolver.load(template_body)
   rendered_body = resolver.render()
 
   if not resolver.resolved_ok():
@@ -116,12 +125,16 @@ def merge_files(context):
 
   if context.verbose:
     print(context)
-    dir_path = normpath(dirname(dest["path"]))
-    print("make directories: {} {}".format(dir_path, dest["dir_perm"]))
-    print("chmod file to: " + dest["file_perm"])
-    user, group = dest["user_group"].split(":")
-    print("chown last directory in path to user: {}, group: {}".format(user, group))
-    print("chown file to user: {}, group: {}\n".format(user, group))
+    if context.no_params:
+      print('no_params flag set to true!')
+      print('Inline template resolution based on external symbol lookup only and no destination for file write.\n')
+    else:
+      dir_path = normpath(dirname(dest["path"]))
+      print("make directories: {} {}".format(dir_path, dest["dir_perm"]))
+      print("chmod file to: " + dest["file_perm"])
+      user, group = dest["user_group"].split(":")
+      print("chown last directory in path to user: {}, group: {}".format(user, group))
+      print("chown file to user: {}, group: {}\n".format(user, group))
 
     print("template body:\n{}\nrendered body:\n{}\n".format(template_body, rendered_body))
   else:
@@ -129,7 +142,7 @@ def merge_files(context):
 
 
 def main():
-  context =  handle_args_and_set_context(sys.argv[1:])
+  context = handle_args_and_set_context(sys.argv[1:])
   try:
     merge_files(context)
   except Exception as e:
